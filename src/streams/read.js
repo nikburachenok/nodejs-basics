e46d2ca941683a8def74ba4827c069c5c355b786
nikburachenok <nikbur19@gmail.com>
2024-01-28 22:03:06 +0100

    feat: done with Streams subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+
+const FILE_PATH = 'src/streams/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(FILE_PATH, 'utf-8');
+
+    readableStream.on('error', function (error) {
+        console.log(`error: ${error.message}`);
+    })
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+
+const FILE_PATH = 'src/streams/files/fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const stream = fs.createWriteStream(FILE_PATH, { flags: 'a' });
+
+    stream.on('error', function (error) {
+        console.log(`error: ${error.message}`);
+    })
+    process.stdin.on("data", content => {
+        content = content.toString();
+        stream.write(content);
+    });
 };
 
 await write();
