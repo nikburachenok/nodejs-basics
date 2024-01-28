0bc0074ad9bbd7b067d6bf831a75afe1026ba5de
nikburachenok <nikbur19@gmail.com>
2024-01-28 21:09:03 +0100

    feat: done with the Child Processes subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { spawn } from 'child_process';
+
+const FILE_PATH = 'src/cp/files/script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [FILE_PATH, ...args]);
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
 };
 
 // Put your arguments in function call to test this functionality
