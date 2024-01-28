10c4a1259936cfedcbc370b075a8b703b92261e2
nikburachenok <nikbur19@gmail.com>
2024-01-28 21:46:35 +0100

    feat: done with the Zlib subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createGzip } from 'zlib';
+import fs from 'fs';
+
+const FILE_PATH = 'src/zip/files/fileToCompress.txt';
+const ARCHIVE_PATH = 'src/zip/files/archive.gz'
+
 const compress = async () => {
-    // Write your code here 
+    fs.createReadStream(FILE_PATH)
+        .pipe(createGzip())
+        .pipe(fs.createWriteStream(ARCHIVE_PATH));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createGzip } from 'zlib';
+import fs from 'fs';
+
+const FILE_PATH = 'src/zip/files/fileToCompress.txt';
+const ARCHIVE_PATH = 'src/zip/files/archive.gz'
+
 const decompress = async () => {
-    // Write your code here 
+    fs.createReadStream(ARCHIVE_PATH)
+        .pipe(createGzip())
+        .pipe(fs.createWriteStream(FILE_PATH))
 };
 
 await decompress();
