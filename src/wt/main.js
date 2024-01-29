705cde0441f3a614797c126013dd7e55b62032dd
nikburachenok <nikbur19@gmail.com>
2024-01-29 12:12:04 +0100

    feat: done with workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
+const FILE_PATH = './src/wt/worker.js';
+
 const performCalculations = async () => {
-    // Write your code here
+
+    let results = [];
+    for (let i = 0; i < cpus().length; i++) {
+        const worker = new Worker(FILE_PATH, { workerData: 10 + i });
+        worker.on("message", (result) => {
+            results[i] = { status: "resolved", data: result };
+            if (i === cpus().length - 1) {
+                console.log(results);
+            }
+        });
+        worker.on("error", () => {
+            results[i] = { status: "error", data: null };
+            if (i === cpus().length - 1) {
+                console.log(results);
+            }
+        })
+    }
 };
 
 await performCalculations();
