cd7145e6517dea08accc0b0ac63c7bfbedf07ff1
nikburachenok <nikbur19@gmail.com>
2024-01-29 19:40:56 +0100

    fix: improved main in the Workers task and added Streams API in the Hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -6,7 +6,10 @@ import { dirname, join } from "path";
 const FILE_PATH = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToCalculateHashFor.txt');
 
 const calculateHash = async () => {
-    console.log(createHash('sha256').update(fs.readFileSync(FILE_PATH, 'utf-8')).digest('hex'));
+    const readableStream = fs.createReadStream(FILE_PATH, 'utf-8');
+    readableStream.on('data', (chunk) => {
+        console.log(createHash('sha256').update(chunk).digest('hex'));
+    })
 };
 
 await calculateHash();
--- src/wt/main.js
+++ src/wt/main.js
@@ -8,17 +8,18 @@ const FILE_PATH = join(dirname(fileURLToPath(import.meta.url)), 'worker.js');
 const performCalculations = async () => {
 
     let results = [];
-    for (let i = 0; i < cpus().length; i++) {
+    let numberOfThreads = cpus().length;
+    for (let i = 0; i < cpus().length; ++i) {
         const worker = new Worker(FILE_PATH, { workerData: 10 + i });
         worker.on("message", (result) => {
             results[i] = { status: "resolved", data: result };
-            if (i === cpus().length - 1) {
-                console.log(results);
-            }
         });
         worker.on("error", () => {
             results[i] = { status: "error", data: null };
-            if (i === cpus().length - 1) {
+        })
+        worker.on('exit', () => {
+            --numberOfThreads;
+            if (!numberOfThreads) {
                 console.log(results);
             }
         })
